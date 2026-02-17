a50659865b001e57ae59e93d1c442a0243d97e37
tomchan1111 <tomchan11@gmail.com>
2026-02-17 06:00:28 +0000

    modified:   api/data.js

--- api/data.js
+++ api/data.js
@@ -40,6 +40,7 @@ export default async function handler(req, res) {
         access: 'public',
         contentType: 'application/json',
         addRandomSuffix: false,
+        allowOverwrite: true,
       });
       return res.status(200).json({ ok: true });
     } catch (error) {
